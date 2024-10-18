82aa4b22302e03702a855c9e21778aed9a2fa82c
Ethan <ethancha1011@gmail.com>
2024-10-18 19:53:35 -0400

    -a

--- script.js
+++ script.js
@@ -1,23 +1,30 @@
 const container = document.querySelector("#container");
-const button = document.querySelector("#generate-container");
-
-function generateContainer(size) {
-  const squareSize = 500 / size;
-
-  for (let i = 0; i < size*size; i++) {
-    const square = document.createElement("div");
-    square.classList.add("square");
-    square.style.width = `${squareSize}px`;
-    square.style.height = `${squareSize}px`;
-
-    /*
-    square.addEventListener("mouseenter", () => {
-      square.style.backgroundColor = "black";
-    });
-    */
-    container.appendChild(square);
-  }
-}
+const button = document.querySelector("#clear");
 
 generateContainer(16);
+function generateContainer(size) {
+    container.innerHTML = "";
+    const squareSize = 500 / size;
+
+    for (let i = 0; i < size*size; i++) {
+        const square = document.createElement("div");
+        square.classList.toggle("square");
+        square.style.width = `${squareSize}px`;
+        square.style.height = `${squareSize}px`;
+
+        square.addEventListener("mouseenter", () => {
+            square.style.backgroundColor = "black";
+        });
+
+        container.appendChild(square);
+    }
+}
+button.addEventListener("click", () =>{
+    const squares = document.querySelectorAll(".square");
+    squares.forEach(square => {
+        square.style.backgroundColor = "white";
+    });
+    generateContainer(16);
+});
+
 
